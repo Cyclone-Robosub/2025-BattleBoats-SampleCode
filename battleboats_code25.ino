8ad4884d935906198633e46a1dccfa99005bb0f3
Stannnley <syzjiang@ucdavis.edu>
2025-10-11 16:33:31 -0700

    Update battleboats_code25.ino
    added the 2nd sensor's reading to this, no logic changed

--- battleboats_code25.ino
+++ battleboats_code25.ino
@@ -9,9 +9,15 @@ void setup() {
 
 void loop() {
   float dist = readDistanceCM();
+  float dist2 = readDistanceCM2();
+  
   Serial.print("Distance: ");
   Serial.print(dist);
-  Serial.println(" cm");
+  Serial.println(" cm");|
+  
+  Serial.println("Distance: ";
+  Serial.print(dist2);
+  Serial.print(" cm");
 
   if (dist > 15) {
     driveForward(150);
