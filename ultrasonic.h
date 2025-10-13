bea550893477efe5cccc59dc3e704338ffbe7474
arclyu-art <arclyu@ucdavis.edu>
2025-10-13 09:08:41 -0700

    Update ultrasonic.h
    Declaring pins and make it more readable

--- ultrasonic.h
+++ ultrasonic.h
@@ -8,6 +8,8 @@
 // ==============================
 #define TRIG_PINFRONT 2
 #define ECHO_PINFRONT 3
+#define TRIG_PINSIDE 8
+#define ECHO_PINSIDE 9
 //there I define the interface of the sensor from the front
 void ultrasonicInit();
 float readDistanceCM();
